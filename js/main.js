227c2397447e7087dfdb23da9d3c0fc5f0ac0385
breakkeyboard <69479788+breakkeyboard@users.noreply.github.com>
2020-09-30 12:09:07 +0500

    site-hosting

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+$('.reviews-slider').slick({
+infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: false,
+  dots: true
+
+});
+$('.btn__menu').on('click',function(){
+$('.menu__list').slideToggle();
+});
+
+
+
+
+});
